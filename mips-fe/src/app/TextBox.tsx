29fd9ac732359a18e44c281d2baaf2802cb03a5f
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2024-02-22 16:08:46 +0530

    WIP

--- mips-fe/src/app/TextBox.tsx
+++ mips-fe/src/app/TextBox.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { run_prog } from "mips_be";
+import { registerCallback, unregisterCallback } from "./regHandler";
+import RegisterDisplay from "./RegisterDisplay";
+
 export default function TextBox() {
   const [state, setState] = useState("");
-
+  const [regFile, setRegFile] = useState<Object>({});
+  useEffect(() => {
+    const doUpdateRegFile = (nf: Array<number>) => {
+      setRegFile(nf);
+    };
+    registerCallback(doUpdateRegFile);
+    return () => {
+      unregisterCallback(doUpdateRegFile);
+    };
+  }, []);
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setState(e.target.value);
   };
@@ -15,6 +27,7 @@ export default function TextBox() {
     <>
       <textarea value={state} onChange={handleChange} />
       <button onClick={doParse}>Parse</button>
+      <RegisterDisplay regFile={regFile} />
     </>
   );
 }
